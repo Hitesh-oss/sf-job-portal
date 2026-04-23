537894b22e28671d4ac54ba3db96dedf15c993e6
Hitesh@Zorin <garnayakhitesh@gmail.com>
2026-04-24 00:37:13 +0530

    Day 3: Added Apply button and application creation

--- force-app/main/default/lwc/jobList/jobList.js
+++ force-app/main/default/lwc/jobList/jobList.js
@@ -1,5 +1,7 @@
 import { LightningElement, wire } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import getAllJobs from '@salesforce/apex/JobPortalController.getAllJobs';
+import createApplication from '@salesforce/apex/JobPortalController.createApplication';
 
 export default class JobList extends LightningElement {
     jobs;
@@ -14,5 +16,30 @@ export default class JobList extends LightningElement {
             this.error = error;
             this.jobs = undefined;
         }
+    }
+
+    handleApply(event) {
+        const jobId = event.target.dataset.id;
+
+        createApplication({ jobId: jobId })
+            .then(() => {
+                this.dispatchEvent(
+                    new ShowToastEvent({
+                        title: 'Success',
+                        message: 'Application Submitted!',
+                        variant: 'success'
+                    })
+                );
+            })
+            .catch(error => {
+                console.error(error);
+                this.dispatchEvent(
+                    new ShowToastEvent({
+                        title: 'Error',
+                        message: 'Application failed',
+                        variant: 'error'
+                    })
+                );
+            });
     }
 }
